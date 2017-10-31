8a5fbd2a2643f1d82f5b10edfb431193ee022bdc
zz1987 <wujiang@duotin.com>
2017-10-31 20:00:13 +0800

    sprint boot init

---
+++ src/main/java/com/zhz/smart/SmartqqApplication.java
@@ -0,0 +1,12 @@
+package com.zhz.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartqqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartqqApplication.class, args);
+	}
+}
